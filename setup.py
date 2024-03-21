77f67572cd42406f4efc746a21fa5b4f9d29f7f1
Gatlen Culp <gatlenculp@gmail.com>
2024-03-21 17:21:45 -0400

    Added setup.py and a requirements folder for venv

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import sys
 
-APP = ['gui.py']
-DATA_FILES = []
-OPTIONS = {
-    'argv_emulation': True,
-    'packages': ['required', 'packages'],
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Standard setup for a Python package
+setup_args = {
+    'name': 'MopMan_PacketMaker',
+    'version': '0.1.0',
+    'packages': find_packages(),
+    'install_requires': requirements,
+    # Add other standard setup parameters as needed
 }
 
-setup(
-    app=APP,
-    data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-)
+# Conditional setup for py2app
+if 'py2app' in sys.argv:
+    setup_args.update({
+        'app': ['your_script.py'],
+        'data_files': [],
+        'options': {
+            'py2app': {
+                'argv_emulation': True,
+                'packages': ['required', 'packages'],
+            }
+        },
+        'setup_requires': ['py2app'],
+    })
+
+setup(**setup_args)
